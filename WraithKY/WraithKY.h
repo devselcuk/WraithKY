d8882faf8d7be86c10605aea36f2a3918b8f5e6e
Selçuk YILDIZ <devselcukdev@gmail.com>
2020-04-23 19:26:26 +0300

    first commit

---
+++ WraithKY/WraithKY.h
@@ -0,0 +1,19 @@
+//
+//  WraithKY.h
+//  WraithKY
+//
+//  Created by Enes on 23.04.2020.
+//  Copyright © 2020 KY. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WraithKY.
+FOUNDATION_EXPORT double WraithKYVersionNumber;
+
+//! Project version string for WraithKY.
+FOUNDATION_EXPORT const unsigned char WraithKYVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WraithKY/PublicHeader.h>
+
+
