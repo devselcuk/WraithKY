1014bfcb509cddd21c312c77a46702d3e206fb80
Selçuk YILDIZ <devselcukdev@gmail.com>
2020-04-23 22:42:02 +0300

    WraithKY.podspec

---
+++ WraithKY/Wraith.swift
@@ -0,0 +1,17 @@
+//
+//  Wraith.swift
+//  WraithKY
+//
+//  Created by Enes on 23.04.2020.
+//  Copyright © 2020 KY. All rights reserved.
+//
+
+import Foundation
+
+class Wraith {
+    
+    
+    func add() {
+        print("added")
+    }
+}
